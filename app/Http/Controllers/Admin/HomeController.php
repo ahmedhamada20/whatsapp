62cd79980eb5c7f1c08c70f7250d193bea5b39bb
‪Ahmed Hamada‬‏ <63430772+ahmedhamada20@users.noreply.github.com>
2023-05-07 12:02:13 +0300

    edi

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -13,11 +13,29 @@ class HomeController extends Controller
 
     public function notifications_webhook(Request $request)
     {
+        // Read the raw POST data from the request
         $payload = $request->getContent();
-        fwrite(STDOUT, print_r($payload, true) . "\n");
+
+        // Print the payload to the log for debugging purposes
+        info($payload);
+
+        // Instantiate the Webhook super class
         $webhook = new WebHook();
-        dd($webhook->read($payload));
-        fwrite(STDOUT, print_r($webhook->read(json_decode($payload, true)), true) . "\n");
+
+        // Parse the payload into an array and pass it to the WebHook class's read method
+        $result = $webhook->read(json_decode($payload, true));
+
+        // Print the result to the log for debugging purposes
+        info(print_r($result, true));
+
+        // Return a response to the incoming request (if needed)
+        return response('OK');
+
+//        $payload = $request->getContent();
+//        fwrite(STDOUT, print_r($payload, true) . "\n");
+//        $webhook = new WebHook();
+//        dd($webhook->read($payload));
+//        fwrite(STDOUT, print_r($webhook->read(json_decode($payload, true)), true) . "\n");
     }
 
     public function sendMessages(Request $request)
